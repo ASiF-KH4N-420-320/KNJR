e5d2e9f4abd3841a594ea5d541d50ba105a06bb9
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2023-02-28 23:37:41 +0500

    Update SK.py

--- SK.py
+++ SK.py
@@ -1,8 +1,8 @@
 import os, platform
 def Run():
         bit = platform.architecture()[0]
-        if bit == '64bit':
-            import jnxcx
+        
+
         elif bit == '32bit':
             import Pro32
 Run()
